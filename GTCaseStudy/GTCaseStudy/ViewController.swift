41957b5eabc243883a4016dae9fa14dc43426f11
Samet Yatmaz <sametytmz@gmail.com>
2025-05-09 14:21:35 +0400

    created project

---
+++ GTCaseStudy/GTCaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GTCaseStudy
+//
+//  Created by Samet Yatmaz on 9.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
